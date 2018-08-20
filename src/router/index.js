6fe0f70aa6756ebcc525af8d4312c19b9d9e687a
Lu Chaohai <luchaohai@cn.ibm.com>
2018-08-20 20:21:03 +0800

    添加tarde_wallet静态页面

--- src/router/index.js
+++ src/router/index.js
@@ -15,13 +15,12 @@ const mt4_create = ()=> import( '../pages/mt4_create.vue')
 const mt4_bind = ()=> import( '../pages/mt4_bind.vue')
 const mt4_modifypwd = ()=> import( '../pages/mt4_modifypwd.vue')
 const mt4_findpwd = ()=> import( '../pages/mt4_findpwd.vue')
-
-
+const trade_wallet = ()=> import( '../pages/trade_wallet.vue')
 
 
 
 /**
- * test part 
+ * test part
  */
 const test1 = () =>  import ('./../pages/test/test1.vue')
 const test2 = () =>  import ('./../pages/test/test2.vue')
@@ -30,7 +29,7 @@ const test4 = () =>  import ('./../pages/test/test4.vue')
 const test5 = () =>  import ('./../pages/test/test5.vue')
 
 /**
- * accounting part (accounting) webpackChunkName: "accounting" 
+ * accounting part (accounting) webpackChunkName: "accounting"
  */
 /**
  * account part
@@ -87,6 +86,7 @@ crmRoutes = crmRoutes.concat(
   newRoute('/mt4_modifypwd', 'mt4_modifypwd', mt4_modifypwd),
   newRoute('/mt4_findpwd', 'mt4_findpwd', mt4_findpwd),
   newRoute('/mt4_create/:server', 'mt4_create', mt4_create),
+  newRoute('/trade_wallet', 'trade_wallet', trade_wallet),
 )
 
 
