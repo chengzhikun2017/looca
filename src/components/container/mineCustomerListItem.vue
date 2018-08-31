a437429df1fcd463aa7319a6587846ba6bd8f94d
Lu Chaohai <luchaohai@cn.ibm.com>
2018-08-31 16:35:08 +0800

    我的客户和返现记录的移动列表项：mineCustomerListItem; rebateListItem

---
+++ src/components/container/mineCustomerListItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="mine_customer_list_item">
+    <div class="mine_customer_list_item-content">
+      <div class="mine_customer_list_item-summary" flex="cross:center">
+        <div class="mine_customer_list_item-summary-title" flex-box="1">
+          姓名：陶*
+        </div>
+        <div class="mine_customer_list_item-summary-title" flex="main:right" flex-box="1">
+          手机号：177****1234
+        </div>
+      </div>
+      <div class="mine_customer_list_item-others" flex="cross:center main:justify">
+        <div class="mine_customer_list_item-others-time" flex-box="1">
+          一级
+        </div>
+        <div class="mine_customer_list_item-others-time" flex="main:right" flex-box="1">
+          2018-12-12 12:12:12
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import {getTimeString} from '@/utils/time.js'
+  export default {
+    name: 'mine_customer_list_item',
+    props: {
+      data: {
+        type: Object,
+        default: () => {}
+      }
+    },
+    data() {
+      return {
+        isDetailShow: false
+      }
+    },
+    methods: {
+      toggle () {
+        this.isDetailShow = !this.isDetailShow
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  $prefix: "mine_customer_list_item";
+  @import '@/styles/listitem/index.scss';
+  .#{$prefix}-others-time {
+    padding-top: 8px !important;
+    font-size: 14px !important;
+  }
+</style>
+
