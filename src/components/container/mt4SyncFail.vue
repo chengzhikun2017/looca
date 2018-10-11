11c532173a10204a3a46c0a80e2d24b3c840d43c
Lu Chaohai <luchaohai@cn.ibm.com>
2018-10-11 15:58:12 +0800

    同步文案更新； 右上角用户信息模块增加（需要对接）；

--- src/components/container/mt4SyncFail.vue
+++ src/components/container/mt4SyncFail.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
-    <a-alert message="同步失败" type="warning" v-if="success" showIcon>
-      <p slot="description">
+    <a-alert message="由于网络延迟导致数据未完全同步，您可以稍后刷新重试或者前往MT4客户端查看" type="warning" v-if="success" showIcon>
+      <!-- <p slot="description">
         <span>
-          最新数据同步失败，如需查看最新数据，请稍后点击刷新尝试，或者前往MT4客户端查看
+
         </span>
         <a-button type="primary" size="small" :icon="icon" @click="refresh">
           刷新
-      </a-button>
-      </p>
+        </a-button>
+      </p> -->
     </a-alert>
   </div>
 </template>
