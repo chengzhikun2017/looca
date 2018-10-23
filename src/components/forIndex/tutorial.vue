ff5cb34f9d46fa6937ba2945460681d5c927694d
Lu Chaohai <luchaohai@cn.ibm.com>
2018-10-23 15:59:36 +0800

    官网文案以及内容更新

---
+++ src/components/forIndex/tutorial.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tutorial">
+    <img class="tutorial-img" v-for="n in pages" :key="n" :src="'https://www.looco8.com/' + file + '/幻灯片' + n + '.jpg'" alt="">
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      file: {
+        type: String,
+        default: ''
+      },
+      pages: {
+        type: Number,
+        default: 0
+      }
+    },
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .tutorial {
+    &-img {
+      width: 100%;
+    }
+  }
+</style>
