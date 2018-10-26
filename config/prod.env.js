095e356660f3ffb6c9c0821358e26ec84c97fafc
Splinter <912143546@qq.com>
2018-10-26 11:16:06 +0800

    分环境打包

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,36 @@
 'use strict'
 const merge = require('webpack-merge')
-const prodShowEnv = require('./prod.show.env.js')
-module.exports =  merge(prodShowEnv, {
-  NODE_ENV: '"production"'
-})
+const crm = {
+  publicPath:'https://looco-crm-1251098434.file.myqcloud.com/',
+  assetsRoot:'../dist_crm',
+  assetsSubDirectory: 'static',
+  project:'crm',
+}
+const show = {
+  publicPath:'https://looco-1251098434.file.myqcloud.com/',
+  assetsRoot:'../dist_show',
+  assetsSubDirectory: 'static',
+  project:'show',
+}
+const crm_test = {
+  publicPath:'/fx/',
+  assetsRoot:'../dist_crm_test',
+  assetsSubDirectory: 'static',
+  project:'crm_test',
+}
+
+const configs = {
+  crm,
+  show,
+  crm_test,
+  default:{
+    publicPath:'/',
+    assetsRoot:'../dist',
+    assetsSubDirectory: 'static',
+    project:'default',
+  }
+}
+module.exports = {
+  NODE_ENV: '"production"',
+  ...configs[process.env.env_config||'default'],
+}
