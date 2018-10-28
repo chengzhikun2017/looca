5e36d8aa20bb17d2112cde6c2f3584eacc6d12b1
Splinter <912143546@qq.com>
2018-10-28 19:14:54 +0800

    helper page htm;

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,58 @@
 #!/usr/bin/expect -f  
-  set user root  
-  set host 118.31.42.82
-  set password Yfz\;Zdxu4l0Uop\$sfU3Ty
-  set timeout -1  
-   
-  spawn ssh $user@$host  
-  expect "*assword:*"  
-  send "$password\r"  
-  interact  
-  expect eof  
+set user root  
+set host 118.31.42.82
+set password Yfz\;Zdxu4l0Uop\$sfU3Ty
+set timeout -1  
+ 
+spawn ssh $user@$host  
+expect "*assword:*"  
+send "$password\r"  
+interact  
+
+# rm -r -f dist_crm_test/crm
+# spawn cp -r dist_crm_test deployCache/crm_test
+# spawn scp dist_crm_test/crm $user@$host:/data/wwwroot/default/crm
+# expect "*password:*"
+# send "$password\r"
+# interact
+
+# echo "-------deploy completed----------"
+  # expect eof  
+
+
+
+
+
+
+
+
+
+
+
+  # Yfz;Zdxu4l0Uop$sfU3Ty
+  # 
+  # 测试服：目前用一分钟的服务器
+# 118.31.42.82
+# root
+# Yfz;Zdxu4l0Uop$sfU3Ty
+
+# 目录：/data/wwwroot/default/fx
+# CRM API地址：http://118.31.42.82/api/
+
+
+#############################################################################
+
+# 正式服：
+# 47.244.46.33
+# root
+# 54aS@es&9EQc22%ws@!
+
+# CRM网址：https://crm.looco8.com
+# CRM目录：/data/wwwroot/crm.looco8.com
+# CRM资源加速域名：https://looco-crm-1251098434.file.myqcloud.com/
+# CRM API地址：https://crm.looco8.com/api/
+
+# 官网网址：https://www.looco8.com
+# 官网目录：/data/wwwroot/www.looco8.com
+# 官网资源加速域名：https://looco-1251098434.file.myqcloud.com/
+
--- src/pages/helper.vue
+++ src/pages/helper.vue
@@ -1,23 +1,105 @@
 <template>
-  <div>
-    helper
+  <div class="helper-page">
+    <div class="helper-page-note">
+      <div class="helper-page-note-title">
+        普通账户
+      </div>
+      <div class="helper-page-note-item">
+        1.普通账户最多可以创建5个；
+      </div>
+      <div class="helper-page-note-item">
+        2.手续费：外汇1手30美金，黄金白银1手60美金，原油1手6美金；
+      </div>
+      <div class="helper-page-note-item">
+        3.交易密码自己交易时使用，只读密码，只能查看，不能交易；
+      </div>
+      <div class="helper-page-note-item">
+        4.普通账户持仓最长不超过1个月，否则平台将强制平仓；
+      </div>
+      <div class="helper-page-note-item">
+        5.如需帮助，请联系客服QQ83166672、电话4000577009。
+      </div>
+    </div>
+    <div class="helper-page-note">
+      <div class="helper-page-note-title">
+        跟单账户
+      </div>
+      <div class="helper-page-note-item">
+        1.跟单账户最多可以创建10个；
+      </div>
+      <div class="helper-page-note-item">
+        2.手续费：外汇1手30美金，黄金白银1手60美金，原油1手6美金；
+      </div>
+      <div class="helper-page-note-item">
+        3.平台获取跟单盈利的25%佣金，直系上级获取跟单盈利的5%佣金，亏损不收取佣金；
+      </div>
+      <div class="helper-page-note-item">
+        4.周期为一个月，每月1号结算上月跟单佣金，若客户月内提现，跟单佣金即时结算；
+      </div>
+      <div class="helper-page-note-item">
+        5.跟单账户需在月末最后一天24点前全部平仓，待结算后中午12点重新开仓；
+      </div>
+      <div class="helper-page-note-item">
+        6.跟单账号自己不可交易，若盈利，平台照常收取跟单佣金，若因自己操作失误造成亏损，平台概不负责；
+      </div>
+      <div class="helper-page-note-item">
+        7.如需帮助，请联系客服QQ83166672、电话4000577009。
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  name:'helper',
+  name: 'helper',
   data() {
     return {
-      
+
     }
   },
   methods: {},
   computed: {},
   components: {},
 }
+
 </script>
+<style lang="scss">
+$prefix: "helper";
+@import '@/styles/utils/cardTable.scss';
+@import '@/styles/utils/note.scss';
+.#{$prefix}-page {
+  .#{$prefix}-content {
+    margin: 20px 0;
+    padding: 20px 0;
+    .#{$prefix}-content-title {
+      font-weight: 800;
+      margin-bottom: 10px;
+    }
+    .#{$prefix}-content-image {
+      max-width: 160px;
+      height: 160px;
+      img {
+        width: 100%;
+      }
+    }
+    .#{$prefix}-content-link {
+      margin-top: 10px;
+      font-size: 15px;
+      font-weight: bold;
+      .link-box{
 
-<style lang='scss' scoped>
+      }
+      .link{
+        font-size: 13px;
+        font-weight: normal;
+      }
+    }
+  }
+}
 
+@media (min-width: 575px) {
+  .#{$prefix}-page {}
+}
+
+@import '@/styles/utils/cardTable.scss';
+@import '@/styles/utils/note.scss';
 </style>
