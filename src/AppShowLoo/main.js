3ccb59290c107c58d75c9b8a7ff4699c40269c85
Splinter <912143546@qq.com>
2018-10-28 20:48:36 +0800

    trim

--- src/AppShowLoo/main.js
+++ src/AppShowLoo/main.js
@@ -5,34 +5,34 @@ import App from './App2.vue'
 import flex from 'flex.css'
 import router from './router.js'
 import {
-  Button,
-  LocaleProvider,
+  // Button,
+  // LocaleProvider,
   Carousel,
   Icon,
-  Menu,
-  Layout,
-  Form,
-  Input,
-  Select,
-  Tabs,
-  Checkbox,
+  // Menu,
+  // Layout,
+  // Form,
+  // Input,
+  // Select,
+  // Tabs,
+  // Checkbox,
   Spin,
-  Message,
+  // Message,
   Modal,
   Dropdown,
-  Upload,
+  // Upload,
   List,
-  Table,
-  TreeSelect,
-  Radio,
-  Col,
-  Row,
-  Alert,
-  Steps,
-  Breadcrumb,
-  DatePicker,
-  Card,
-  Divider,
+  // Table,
+  // TreeSelect,
+  // Radio,
+  // Col,
+  // Row,
+  // Alert,
+  // Steps,
+  // Breadcrumb,
+  // DatePicker,
+  // Card,
+  // Divider,
   Popover
 } from 'vue-antd-ui';
 Vue.config.productionTip = false
@@ -46,51 +46,51 @@ function registerComponentChild(vue, parentAntComponent) {
   }
 }
 
-Vue.component(Button.name, Button)
-Vue.component(LocaleProvider.name, LocaleProvider)
+// Vue.component(Button.name, Button)
+// Vue.component(LocaleProvider.name, LocaleProvider)
 Vue.component(Modal.name, Modal)
 Vue.component(Popover.name, Popover)
-Vue.component(Upload.name, Upload)
-registerComponentChild(Vue, Upload)
+// Vue.component(Upload.name, Upload)
+// registerComponentChild(Vue, Upload)
 Vue.component(Spin.name, Spin)
-Vue.component(Input.name, Input)
+// Vue.component(Input.name, Input)
 Vue.component(Dropdown.name, Dropdown)
-registerComponentChild(Vue, Input)
+// registerComponentChild(Vue, Input)
 Vue.component(Icon.name, Icon)
 Vue.component(Carousel.name, Carousel)
-Vue.component(Checkbox.name, Checkbox)
-Vue.component(Menu.name, Menu)
-registerComponentChild(Vue, Menu)
-Vue.component(Layout.name, Layout)
-registerComponentChild(Vue, Layout)
+// Vue.component(Checkbox.name, Checkbox)
+// Vue.component(Menu.name, Menu)
+// registerComponentChild(Vue, Menu)
+// Vue.component(Layout.name, Layout)
+// registerComponentChild(Vue, Layout)
 Vue.component(List.name, List)
 registerComponentChild(Vue, List)
-Vue.component(Select.name, Select)
-registerComponentChild(Vue, Select)
-Vue.component(Tabs.name, Tabs)
-registerComponentChild(Vue, Tabs)
-Vue.component(Form.name, Form)
-registerComponentChild(Vue, Form)
-Vue.component(Table.name, Table)
-registerComponentChild(Vue, Table)
-Vue.component(TreeSelect.name, TreeSelect)
-registerComponentChild(Vue, Radio)
-Vue.component(Radio.name, Radio)
-registerComponentChild(Vue, TreeSelect)
-registerComponentChild(Vue, Col)
-Vue.component(Col.name, Col)
-registerComponentChild(Vue, Row)
-Vue.component(Row.name, Row)
-Vue.component(Alert.name, Alert)
-registerComponentChild(Vue, Steps)
-Vue.component(Steps.name, Steps)
-registerComponentChild(Vue, Alert)
-Vue.component(Breadcrumb.name, Breadcrumb)
-registerComponentChild(Vue, Breadcrumb)
-Vue.component(DatePicker.name, DatePicker)
-registerComponentChild(Vue, DatePicker)
-Vue.component(Divider.name, Divider)
-Vue.component(Card.name, Card)
+// Vue.component(Select.name, Select)
+// registerComponentChild(Vue, Select)
+// Vue.component(Tabs.name, Tabs)
+// registerComponentChild(Vue, Tabs)
+// Vue.component(Form.name, Form)
+// registerComponentChild(Vue, Form)
+// Vue.component(Table.name, Table)
+// registerComponentChild(Vue, Table)
+// Vue.component(TreeSelect.name, TreeSelect)
+// registerComponentChild(Vue, Radio)
+// Vue.component(Radio.name, Radio)
+// registerComponentChild(Vue, TreeSelect)
+// registerComponentChild(Vue, Col)
+// Vue.component(Col.name, Col)
+// registerComponentChild(Vue, Row)
+// Vue.component(Row.name, Row)
+// Vue.component(Alert.name, Alert)
+// registerComponentChild(Vue, Steps)
+// Vue.component(Steps.name, Steps)
+// registerComponentChild(Vue, Alert)
+// Vue.component(Breadcrumb.name, Breadcrumb)
+// registerComponentChild(Vue, Breadcrumb)
+// Vue.component(DatePicker.name, DatePicker)
+// registerComponentChild(Vue, DatePicker)
+// Vue.component(Divider.name, Divider)
+// Vue.component(Card.name, Card)
 
 /* eslint-disable no-new */
 new Vue({
